72ee6b2516133f02225d2f130643acdc7957ae6d
Repaka Shanmukhi <shanmukhirepaka@gmail.com>
2025-04-09 10:29:38 +0000

    changes

--- webapp/controller/Test.controller.js
+++ webapp/controller/Test.controller.js
@@ -1,35 +1,65 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], (Controller) => {
+    "sap/ui/core/mvc/Controller",
+    "sap/m/ColumnListItem",
+    "sap/m/Text",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator"
+], (Controller, ColumnListItem, Text, Filter, FilterOperator) => {
     "use strict";
-     var that;
+    var that;
     return Controller.extend("sample.controller.Test", {
         onInit() {
-            that=this;
-            var oResourceModel = this.getOwnerComponent().getModel("i18n");
+            that = this;
+            var oResourceModel = that.getOwnerComponent().getModel("i18n");
             var oResourceBundle = oResourceModel.getResourceBundle();
             var sTitle = oResourceBundle.getText("title");
-            this.byId("Text").setText(sTitle);
+            that.byId("Text").setText(sTitle);
             var sGreeting = oResourceBundle.getText("greeting");
-            this.byId("greetingText").setText(sGreeting)
+            that.byId("greetingText").setText(sGreeting);
 
-            var oUnique= that.getOwnerComponent().getModel();
-            oUnique.read("/EmployeeInfo",{
+            // Fetching the data without binding in view using id's 
+            var oUnique = that.getOwnerComponent().getModel();
+            var sDepartment = "SAP UI5"; 
+            var oDepartmentFilter = new Filter("Department", FilterOperator.EQ, sDepartment);
+            oUnique.read("/EmployeeInfo", {
+                filters: [oDepartmentFilter], 
                 success: function (oData) {
-                var uniqueDepartments = [];
-                oData.results.forEach(function (employee) {
-                    var department = employee.Department;
-                    
-                })
-            }
-            })
+                    console.log(oData);  
+                    var oEmployeeModel = new sap.ui.model.json.JSONModel(oData.results);  
+                    var oTable = that.getView().byId("employeeTable");
+                    oTable.setModel(oEmployeeModel);
+                    oTable.bindItems({
+                        path: "/",  
+                        template: new ColumnListItem({
+                            cells: [
+                                new Text({ text: "{ID}" }),
+                                new Text({ text: "{FirstName} {LastName}" }),
+                                new Text({ text: "{Email}" }),
+                                new Text({ text: "{Phone}" }),
+                                new Text({ text: "{BloodGroup}" }),
+                                new Text({ text: "{Department}" }),
+                                new Text({ text: "{Position}" }),
+                                new Text({ text: "{Salary}" }),
+                                new Text({ text: "{path: 'JoiningDate', formatter: '.formatJoiningDate'}" })
+                            ]
+                        })
+                    });
+                },
+                error: function (oError) {
+                    console.log("Error fetching data: ", oError);
+                }
+            });
         },
+
         formatJoiningDate: function (sDate) {
             if (sDate) {
                 var oDate = new Date(sDate);
-                var oFormatter = sap.ui.core.format.DateFormat.getDateInstance({pattern: "yyyy-MM-dd"});
+                var oFormatter = sap.ui.core.format.DateFormat.getDateInstance({ pattern: "yyyy-MM-dd" });
                 return oFormatter.format(oDate);
             }
-        },
+        }
     });
 });
+
+
+
