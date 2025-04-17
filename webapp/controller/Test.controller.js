01e8707d1f4edba2b8db17bfd4e5f9b16dc025a2
Repaka Shanmukhi <shanmukhirepaka@gmail.com>
2025-04-17 09:50:53 +0000

    commit

--- webapp/controller/Test.controller.js
+++ webapp/controller/Test.controller.js
@@ -25,6 +25,41 @@ sap.ui.define([
                 }
             }); 
         }, 
+        onInputChange: function (oEvent) {
+            var oInput = oEvent.getSource();
+            var sId = oEvent.getSource().getId();
+            var sValue = oInput.getValue();
+            var oInput1 = this.byId("input1");
+            var oInput2 = this.byId("input2");
+            var oInput3 = this.byId("input3");
+            [oInput1, oInput2, oInput3].forEach(function(input) {
+                input.setValueState("None");
+            });
+            if (!Number.isInteger(+sValue)) {
+                oInput.setValueState("Error");
+                oInput.setValueStateText("Please enter only integer values.");
+            } else {
+                var iValue = parseInt(sValue);
+                if (iValue < 0) {
+                    oInput.setValueState("Error");
+                    oInput.setValueStateText("Number should be greater than or equal to 0.");
+                } else if (iValue > 104) {
+                    oInput.setValueState("Error");
+                    oInput.setValueStateText("Number should be less than or equal to 104.");
+                } else if (sId.includes("input2") && iValue % 4 !== 0) {
+                    oInput.setValueState("Error");
+                    oInput.setValueStateText("Number should be divisible by 4.");
+                } else if (sId.includes("input3") && iValue % 12 !== 0) {
+                    oInput.setValueState("Error");
+                    oInput.setValueStateText("Number should be divisible by 12.");
+                }
+            }
+            if (oInput.getValueState() === "Error") {
+                oInput1.setValueState("Error");
+                oInput2.setValueState("Error");
+                oInput3.setValueState("Error");
+            }
+        },
         onNavigation: function (oEvent) {
             var oSelectedItem = oEvent.getParameter("listItem");
             var oContext = oSelectedItem.getBindingContext();
@@ -36,5 +71,19 @@ sap.ui.define([
         }
     }); 
 }); 
+// To filter the data based on departments and display it on a table
+//             var oModel = that.getOwnerComponent().getModel();
+//             var oFilter = new Filter("Department", FilterOperator.EQ, "SAP UI5");
+//             oModel.read("/EmployeeInfo", {
+//                 filters: [oFilter],
+//                 success: function (oData) {
+//                     var oEmployeeModel = new sap.ui.model.json.JSONModel({ event: oData.results });
+//                     that.byId("employeeTable").setModel(oEmployeeModel);
+//                 },
+//                 error: function (oError) {
+//                     console.log("Error fetching data: ", oError);
+//                 }
+//             });
+
 
 
--- webapp/controller/Test1.controller.js
+++ webapp/controller/Test1.controller.js
@@ -2,21 +2,21 @@ sap.ui.define([
     "sap/ui/core/mvc/Controller"
 ], function (Controller) {
     "use strict";
-
+    var that;
     return Controller.extend("sample.controller.Test1", {
         onInit: function () {
+            that=this;
             var oRouter = this.getOwnerComponent().getRouter();
             var oRoute = oRouter.getRoute("Test1");
             oRoute.attachPatternMatched(this._onRouteMatched, this);
         },
-
         _onRouteMatched: function (oEvent) {
             var oArgs = oEvent.getParameter("arguments");
             var employeeId = oArgs.employeeId; 
             var oModel = this.getOwnerComponent().getModel();
             oModel.read("/EmployeeInfo", {
                 success: function (oData) {
-                    var oEmployee = oData.results.find(emp => emp.ID === employeeId);
+                        var oEmployee = oData.results.find(emp => emp.ID === employeeId);
                         var oEmployeeModel = new sap.ui.model.json.JSONModel(oEmployee);
                         this.getView().setModel(oEmployeeModel, "employeeDetails");
                 }.bind(this),
@@ -66,58 +66,48 @@ sap.ui.define([
                     oDialog.destroy();
                 }
             });
-        
             oDialog.setModel(oEmployeeModel, "employeeDetails");
             oDialog.open();
         },          
-        onEmergencyContact: function () {
+        onEmergencyContact: function () {        
+            var that = this;
+            if (!that.PersonalInfo) {
+                that.PersonalInfo = sap.ui.xmlfragment("sample.Fragments.Employe", that);
+            }
+            that.getView().addDependent(that.PersonalInfo); 
+            var oFormData = new sap.ui.model.json.JSONModel({
+                ContactName: "",
+                ContactPhone: "",
+                ContactEmail: "",
+                Relationship: ""
+            });
+            that.PersonalInfo.setModel(oFormData, "formData"); 
             var oEmpModel = this.getView().getModel("employeeDetails");
-            var employeeEmail = oEmpModel.getProperty("/Email"); 
+            var employeeEmail = oEmpModel.getProperty("/Email");
             var oModel = this.getOwnerComponent().getModel();
+            
             oModel.read("/EmployeeInfoEmergencyContact", {
-                filters: [new sap.ui.model.Filter("ContactEmail", sap.ui.model.FilterOperator.EQ, employeeEmail)],  
+                filters: [new sap.ui.model.Filter("ContactEmail", sap.ui.model.FilterOperator.EQ, employeeEmail)],
                 success: function (oData) {
                     if (oData.results && oData.results.length > 0) {
                         var oEmergencyContact = oData.results[0];
-                        var oEmergencyContactModel = new sap.ui.model.json.JSONModel(oEmergencyContact);
-                        var oDialog = new sap.m.Dialog({
-                            title: "Emergencv Contact Information",
-                            content: new sap.ui.layout.form.SimpleForm({
-                                editable: false,
-                                layout: "ResponsiveGridLayout",
-                                content: [
-                                    new sap.m.Label({ text: "Contact Name" }),
-                                    new sap.m.Text({ text: "{/ContactName}" }),
-                        
-                                    new sap.m.Label({ text: "Contact Phone" }),
-                                    new sap.m.Text({ text: "{/ContactPhone}" }),
-                        
-                                    new sap.m.Label({ text: "Relationship" }),
-                                    new sap.m.Text({ text: "{/Relationship}" }),
-                                ]
-                        
-                            }),
-                            beginButton: new sap.m.Button({
-                                text: "Close",
-                                press: function () {
-                                    oDialog.close();
-                                }
-                            }),
-                            afterClose: function () {
-                                oDialog.destroy();
-                            }
+                        oFormData.setData({
+                            ContactName: oEmergencyContact.ContactName,
+                            ContactPhone: oEmergencyContact.ContactPhone ,
+                            ContactEmail: oEmergencyContact.ContactEmail,
+                            Relationship: oEmergencyContact.Relationship
                         });
-        
-                        oDialog.setModel(oEmergencyContactModel);
-                        oDialog.open();
                     }
                 },
-                 
                 error: function (oError) {
                     console.log("Error fetching emergency contact: ", oError);
                     sap.m.MessageToast.show("Error fetching emergency contact information.");
                 }
             });
+            that.PersonalInfo.open();
+        },
+        onCancleDialog: function(){
+            that.PersonalInfo.close();
         }
         
     });
