0486dded792e16e3fb3ac252d616fab61f3608a1
Repaka Shanmukhi <shanmukhirepaka@gmail.com>
2025-05-30 13:41:24 +0000

    commit

--- webapp/controller/Test1.controller.js
+++ webapp/controller/Test1.controller.js
@@ -6,77 +6,78 @@ sap.ui.define([
     "sap/ui/core/format/DateFormat",
 ], function (Controller, JSONModel, Filter, FilterOperator, DateFormat) {
     "use strict";
-
+    var that;
     return Controller.extend("sample.controller.Test1", {
         onInit: function () {
-            var oRouter = this.getOwnerComponent().getRouter();
-            oRouter.getRoute("Test1").attachPatternMatched(this._onRouteMatched, this);
+            that = this;
+            var oRouter = that.getOwnerComponent().getRouter();
+            oRouter.getRoute("Test1").attachPatternMatched(that._onRouteMatched, that);
         },
 
         _onRouteMatched: function (oEvent) {
             var employeeId = oEvent.getParameter("arguments").employeeId;
-            var oModel = this.getOwnerComponent().getModel();
+            var oModel = that.getOwnerComponent().getModel();
 
             // --- Load EmployeeInfo ---
             oModel.read("/EmployeeInfo", {
                 success: function (oData) {
                     var employee = oData.results.find(emp => emp.ID === employeeId);
                     if (employee) {
-                        this.getView().setModel(new JSONModel({items: employee}), "employeeDetails");
-                        this._loadEmergencyContact(employeeId);
-                        this._loadLeaveSummary(employeeId);
-                        this._loadLeaveLogs(employeeId);
-                        this._loadPayslips(employeeId);
+                        that.getView().setModel(new JSONModel({items: employee}), "employeeDetails");
+                        that._loadEmergencyContact(employeeId);
+                        that._loadLeaveSummary(employeeId);
+                        that._loadLeaveLogs(employeeId);
+                        that._loadPayslips(employeeId);
                     }
-                }.bind(this),
+                }.bind(that),
                 error: function (err) {
                     console.log("Error fetching EmployeeInfo:", err);
                 }
             });
         },
         _loadEmergencyContact: function (employeeId) {
-            var oModel = this.getOwnerComponent().getModel();
+            var oModel = that.getOwnerComponent().getModel();
             oModel.read("/EmployeeInfoEmergencyContact", {
                 filters: [new Filter("EmployeeID", FilterOperator.EQ, employeeId)],
                 success: function (oData) {
-                    this.getView().setModel(new JSONModel({items: oData.results}), "emergencyContact");
-                }.bind(this),
+                    that.getView().setModel(new JSONModel({items: oData.results}), "emergencyContact");
+                }.bind(that),
                 error: function (err) {
                     console.log("Error fetching Emergency Contact:", err);
                 }
             });
         },
         _loadLeaveSummary: function (employeeId) {
-            var oModel = this.getOwnerComponent().getModel();
+            var oModel = that.getOwnerComponent().getModel();
             oModel.read("/EmployeeLeaveSet", {
                 filters: [new Filter("EmployeeID_ID", FilterOperator.EQ, employeeId)],
                 success: function (oData) {
-                this.getView().setModel(new JSONModel({items: oData.results}), "leaveSummary");
-                }.bind(this),
+                that.getView().setModel(new JSONModel({items: oData.results}), "leaveSummary");
+                }.bind(that),
                 error: function (err) {
                     console.log("Error fetching Leave Summary:", err);
                 }
             });
         },
         _loadLeaveLogs: function (employeeId) {
-            var oModel = this.getOwnerComponent().getModel();
+            var oModel = that.getOwnerComponent().getModel();
             oModel.read("/EmployeeLeaveLog", {
                 filters: [new Filter("EmployeeID_ID", FilterOperator.EQ, employeeId)],
                 success: function (oData) {
-                    this.getView().setModel(new JSONModel(oData.results), "leaveLogs");
-                }.bind(this),
+                    that.getView().setModel(new JSONModel(oData.results), "leaveLogs");
+                }.bind(that),
                 error: function (err) {
                     console.log("Error fetching Leave Logs:", err);
                 }
             });
         },
         _loadPayslips: function (employeeId) {
-            var oModel = this.getOwnerComponent().getModel();
+            var oModel = that.getOwnerComponent().getModel();
             oModel.read("/EmployeePayslips", {
                 filters: [new Filter("EmployeeID_ID", FilterOperator.EQ, employeeId)],
                 success: function (oData) {
-                    this.getView().setModel(new JSONModel(oData.results), "payslips");
-                }.bind(this),
+                    that.getView().setModel(new JSONModel(oData.results), "payslips");
+                }.bind(that),
                 error: function (err) {
                     console.log("Error fetching Payslips:", err);
                 }
@@ -99,5 +100,8 @@ sap.ui.define([
                     return "Warning"; 
             } 
         },
+        onNavBack: function(){
+            that.getOwnerComponent().getRouter().navTo("Test")
+        }
     });
 });
